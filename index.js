d24332a6bf4cdcb87ef79f0df015e9e92283b57b
smnielz <110817166+smnielz@users.noreply.github.com>
2022-08-12 20:16:07 +0200

    Add files from scrimba 9. basketball scoreboard

---
+++ index.js
@@ -0,0 +1,39 @@
+let homeEl = document.getElementById("home-block")
+let home = 0
+function home1()
+{
+    home += 1
+    homeEl.textContent = home
+}
+
+function home2()
+{
+    home += 2
+    homeEl.textContent = home
+}
+
+function home3()
+{
+    home += 3
+    homeEl.textContent = home
+}
+
+let guestEl = document.getElementById("guest-block")
+let guest = 0
+function guest1()
+{
+    guest += 1
+    guestEl.textContent = guest
+}
+
+function guest2()
+{
+    guest += 2
+    guestEl.textContent = guest
+}
+
+function guest3()
+{
+    guest += 3
+    guestEl.textContent = guest
+}
